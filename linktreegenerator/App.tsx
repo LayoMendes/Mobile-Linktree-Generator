9b837383305b2eca24075b29fb9d209db21f383b
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2025-11-27 21:54:55 +0000

    projeto-pronto

--- linktreegenerator/App.tsx
+++ linktreegenerator/App.tsx
@@ -1,96 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+// App.tsx
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FormScreen from './src/screens/FormScreen';
+import LinktreeScreen from './src/screens/LinktreeScreen';
+
+export type RootStackParamList = {
+  Form: undefined;
+  Linktree: {
+    photo: string;
+    whatsapp: string;
+    linkedin: string;
+    github: string;
+    portfolio: string;
+  };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <LinearGradient
-      colors={['#5a5a5a', '#000']}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 1 }}
-      style={styles.container}
-    >
-      <View style={styles.content}>
-        <Text style={styles.title}>Linktree Generator</Text>
-
-
-      <Text style={styles.descricao}>Seja bem vindo ao Linktree Generator! Preencha os campos abaixo e clique em gerar para gerar seu portifolio!</Text>
-         <TextInput
-          placeholder="Sua foto de perfil"
-          placeholderTextColor="#ccc"
-          style={styles.input}
-        />
-
-        <TextInput
-          placeholder="Seu número do WhatsApp"
-          placeholderTextColor="#ccc"
-          style={styles.input}
-        />
-
-
-
-        <TextInput
-          placeholder="Link do seu LinkedIn"
-          placeholderTextColor="#ccc"
-          style={styles.input}
-        />
-
-        <TextInput
-          placeholder="Link do seu GitHub"
-          placeholderTextColor="#ccc"
-          style={styles.input}
-        />
-
-        <TextInput
-          placeholder="Link do seu Portfólio"
-          placeholderTextColor="#ccc"
-          style={styles.input}
-        />
-      </View>
-        <Text style={styles.developed}>Developed by Layo Mendes</Text>
-      <StatusBar style="light" />
-    </LinearGradient>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false, // tiramos header padrão
+        }}
+      >
+        <Stack.Screen name="Form" component={FormScreen} />
+        <Stack.Screen name="Linktree" component={LinktreeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  developed:{
-    color: "grey",
-    margin: 20,
-  },
 
-  descricao:{
-    color: "white",
-    fontSize: 13,
-    margin: 20,
-  },
-  content: {
-    width: "85%",
-    backgroundColor: "#1c1c1c",
-    padding: 20,
-    borderRadius: 20,
-    elevation: 5,
-  },
-
-  title: {
-    fontSize: 32,
-    color: "white",
-    marginBottom: 20,
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-
-  input: {
-    backgroundColor: "#303030",
-    color: "white",
-    padding: 12,
-    marginBottom: 14,
-    borderRadius: 10,
-    fontSize: 16,
-  },
-});
